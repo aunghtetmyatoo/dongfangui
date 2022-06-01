ed3a9b299d330f9e0fd6cfb87b48dcb67365c297
aunghtetmyatoo <aunghtetmyatoo888@gmail.com>
2022-06-01 11:54:43 +0630

    modified nav

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,44 @@
 (function ($) {
-    // $('.accordion > li:eq(0) a').addClass('active').next().slideDown();
+
+    /**
+   * Easy selector helper function
+   */
+    const select = (el, all = false) => {
+        el = el.trim()
+        if (all) {
+            return [...document.querySelectorAll(el)]
+        } else {
+            return document.querySelector(el)
+        }
+    }
+
+    /**
+   * Easy on scroll event listener 
+   */
+    const onscroll = (el, listener) => {
+        el.addEventListener('scroll', listener)
+    }
+
+
+    /**
+   * Toggle .header-scrolled class to #header when page is scrolled
+   */
+    let selectHeader = select('#header')
+    if (selectHeader) {
+        const headerScrolled = () => {
+            if (window.scrollY > 100) {
+                selectHeader.classList.add('header-scrolled')
+            } else {
+                selectHeader.classList.remove('header-scrolled')
+            }
+        }
+        window.addEventListener('load', headerScrolled)
+        onscroll(document, headerScrolled)
+    }
+
+    /**
+   * Accordion
+   */
 
     $(".accordion a").click(function (j) {
         var dropDown = $(this).closest("li").find("p");
@@ -30,24 +69,7 @@
         return false;
     });
 
-    /**
-     * Easy selector helper function
-     */
-    const select = (el, all = false) => {
-        el = el.trim();
-        if (all) {
-            return [...document.querySelectorAll(el)];
-        } else {
-            return document.querySelector(el);
-        }
-    };
 
-    /**
-     * Easy on scroll event listener
-     */
-    const onscroll = (el, listener) => {
-        el.addEventListener("scroll", listener);
-    };
 
     /**
      * Navbar links active state on scroll
